295d79369ac96913606fa2d2e9dac8c8ad9ff7dc
Ilja <tadden12@gmail.com>
2021-09-16 16:00:34 +0300

    finish

--- js/task1.js
+++ js/task1.js
@@ -1,12 +1,10 @@
 // TASK 1
 
-
 const delay = ms => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(ms)
-      }, ms)
-    })
+  return new Promise((resolve) => {
+    setTimeout(() =>
+      resolve(ms), ms)
+  });
 }
 
 const logger = time => console.log(`Resolved after ${time}ms`);
@@ -15,3 +13,6 @@ const logger = time => console.log(`Resolved after ${time}ms`);
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
+
+
+
